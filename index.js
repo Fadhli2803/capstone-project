12835cdc8cfb66caa19e26ac2e0eef1e1f27871d
Muhammad Fadhli Al Hafizh <mfadhli2803@gmail.com>
2025-05-07 13:43:02 +0700

    edit file index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const hamburgerButton = document.getElementById('hamburger-button');
+const closeButton = document.getElementById('close-button');
+const navbarContainer = document.getElementById("navbar-container");
+const navElement = document.getElementById('nav-element');
+
+hamburgerButton.addEventListener("click", () => {
+    // navElement.style.transform = 'translateY(-200%)';
+    // navElement.classList.add("sidebar");
+})
